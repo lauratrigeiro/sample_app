90c14b8acfb085b7aa5378134b1a1383da58c965
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-10-08 00:14:21 -0700

    Complete Chapter 9 Exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,18 +6,34 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # end
 	def setup
 		@base_title = "Ruby on Rails Tutorial Sample App"
+    @user = users(:michael)
 	end
 	
-  test "layout links" do
+  test "layout links for user that is not logged in" do
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path
   	assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", contact_path
   	assert_select "a[href=?]", signup_path
   end
 
+  test "layout links for user that is logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
   test "signup page" do
   	get signup_path
   	assert_select "title", "Sign up | #{@base_title}"
