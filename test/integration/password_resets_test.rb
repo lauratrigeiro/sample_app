4253990bb1ec2373e86dbb71c4307dce3f074cb3
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-10-10 13:09:13 -0700

    Complete Ch. 10 exercises

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -53,4 +53,20 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
   				password_confirmation: 	"foobaz" }
   	assert_template 'users/show'
   end
+
+  test "expired token" do
+  	get new_password_reset_path
+	# Valid submission
+  	post password_resets_path, password_reset: { email: @user.email }
+  	# Get the user from the create action.
+  	@user = assigns(:user)
+  	@user.update_attribute(:reset_sent_at, 3.hours.ago)
+  	patch password_reset_path(@user.reset_token),
+  		email: @user.email,
+  		user: { password: 				"foobar",
+  				password_confirmation: 	"foobar" }
+  	assert_response :redirect
+  	follow_redirect!
+  	assert_match /expired/i, response.body
+  end
 end
