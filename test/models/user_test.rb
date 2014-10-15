26a0fcfc4416ac0ba4a5363598ca81325a92f0cf
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-10-14 20:02:43 -0700

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,4 +66,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "associated miroposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
